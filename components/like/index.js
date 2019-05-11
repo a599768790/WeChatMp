548b6636b268cab4b7d1a5ee8d4c3e70119f857b
Fe_lyh <599768790@qq.com>
2019-05-11 20:53:45 +0800

    2053三元运算符/数据绑定

--- components/like/index.js
+++ components/like/index.js
@@ -11,13 +11,17 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    like:true,
+    yesSrc:"images/like.png",
+    noSrc:"images/dislike.png"
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    clickView:function(e){
+      console.log(e)
+    }
   }
 })
