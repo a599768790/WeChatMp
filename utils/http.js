f44e46f88a17b0b0a30d23d276212fca714a04c2
Fe_lyh <599768790@qq.com>
2019-05-12 22:25:10 +0800

    2224/封装私有方法：错误提示方法

--- utils/http.js
+++ utils/http.js
@@ -1,5 +1,11 @@
 import {config} from '../config.js'
 
+const tips = {
+  1:'出现一个错误!',
+  1005:'appkey错误',
+  3000:'期刊不存在'
+}
+
 class HTTP {
   request(parms){
     if (!parms.method) {
@@ -14,16 +20,30 @@ class HTTP {
         'appkey':config.appkey
       },
       success:(res) => {
-        let code = res.statusCode
-        if(code.startsWhit('2')){
-
+        let code = res.statusCode.toString()
+        if(code.startsWith('2')){
+            parms.success(res)//需要调用，才能执行这个函数
         }else{
-
+          //console.log(res)
+          let error_code = res.data.error_code
+          this._show_error(error_code)//
         }
       },
       fail:(err) => {
-
+        
       }
+    })
+  }
+
+  _show_error(error_code){
+    if(!error_code){
+      error_code = 1
+    }
+    //出现客户端错误
+    wx.showToast({
+      title: tips[error_code],
+      icon: 'none',
+      duration: 2000,//显示时间
     })
   }
 }
